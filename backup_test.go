340e62880d6f1a9fcdb711e2353c96b6ddf3892f
Phil Story <mtps@users.noreply.github.com>
2021-07-01 13:59:40 -0600

    Prevent double copy of backup data. (#10)
    * Prevent double copy of backup data.

--- backup_test.go
+++ backup_test.go
@@ -36,6 +36,42 @@ func (s *upgradeTestSuite) TestBackupDataDir() {
 	s.Require().FileExists(keep)
 }
 
+func (s *upgradeTestSuite) TestNoDoubleBackupDataDir() {
+	home := copyTestData(s.T(), "validate")
+	data := fmt.Sprintf("%s/%s", home, "data")
+	cfg := &cosmovisor.Config{Home: home, Name: "dummyd", DataDir: data}
+	info := &cosmovisor.UpgradeInfo{Name: "chain2"}
+
+	err := cosmovisor.DoUpgrade(cfg, info)
+	s.Require().NoError(err)
+	// Backup dir should now exist.
+	backupDir := cfg.BackupDir(info.Name)
+	s.Require().DirExists(backupDir)
+	// Verify copied files exist.
+	appDbName := fmt.Sprintf("%s/%s", backupDir, "data/application.db")
+	s.Require().FileExists(appDbName)
+	appBz, err := ioutil.ReadFile(appDbName)
+	s.Require().NoError(err)
+	s.Require().Equal(string(appBz), "test\n")
+	stateDbName := fmt.Sprintf("%s/%s", backupDir, "data/modulesDir/state.db")
+	s.Require().FileExists(stateDbName)
+	stateBz, err := ioutil.ReadFile(stateDbName)
+	s.Require().NoError(err)
+	s.Require().Equal(string(stateBz), "test\n")
+	// Verify keep file exists.
+	keep := fmt.Sprintf("%s/%s", backupDir, ".keep")
+	s.Require().FileExists(keep)
+	// Remove backup but leave keep file.
+	err = os.RemoveAll(backupDir + "/data")
+	s.Require().NoError(err)
+	s.Require().FileExists(keep)
+	// Verify data not copied again.
+	err = cosmovisor.DoUpgrade(cfg, info)
+	s.Require().NoError(err)
+	// Backup dir should not exist.
+	s.Require().NoDirExists(backupDir + "/data")
+}
+
 func (s *upgradeTestSuite) TestNoBackupDataDir() {
 	home := copyTestData(s.T(), "validate")
 	cfg := &cosmovisor.Config{Home: home, Name: "dummyd", DataDir: ""}
